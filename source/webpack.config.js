0c6fac7a046ad86510686380b0eeaeb20c252e02
Deuzu <deuzubis@gmail.com>
2018-05-15 21:43:35 +0200

    refacto(source, ci): Rename source folder + Update ci conf

--- source/webpack.config.js
+++ source/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        include: /src/,
         use: {
           loader: 'babel-loader',
         },
